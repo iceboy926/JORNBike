7f275927ee792e7c2f050ee981cc5fb0ec0e649d
mandos <anddy926@163.com>
2016-11-28 11:41:24 +0800

    添加微信类服务

---
+++ JOBicycle/ios/CoMoBicycle/AppInit/SDKInit/WeiXinServer.h
@@ -0,0 +1,13 @@
+//
+//  WeiXinServer.h
+//  CoMoBicycle
+//
+//  Created by 金玉衡 on 16/11/28.
+//  Copyright © 2016年 AutoMo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeiXinServer : NSObject<UIApplicationDelegate>
+
+@end
---
+++ JOBicycle/ios/CoMoBicycle/AppInit/SDKInit/WeiXinServer.m
@@ -0,0 +1,24 @@
+//
+//  WeiXinServer.m
+//  CoMoBicycle
+//
+//  Created by 金玉衡 on 16/11/28.
+//  Copyright © 2016年 AutoMo. All rights reserved.
+//
+
+#import "WeiXinServer.h"
+
+@interface WeiXinServer()
+
+@end
+
+@implementation WeiXinServer
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    
+    
+    return YES;
+}
+
+@end
